5a314feabbb033e5705b23d06c4c5e390ed51992
nibraaska <nnibraas@gmail.com>
2018-11-03 14:32:31 -0500

    Initial set up 1.2

--- src/main/java/com/nibraas/betterme/MainActivity.java
+++ src/main/java/com/nibraas/betterme/MainActivity.java
@@ -58,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, RegisterActivity.class));
             }
         });
-
     }
 
     private void userLogin(){
